0c4567fdf76c0516dc6ac8d75045857ffa83035f
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-08-06 12:49:21 -0600

    learing about dynamic routes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,7 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function index($id){
+        return view('note.index', compact('id'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/note/{id}', [NoteController::class, 'index'])->name('note.index');
