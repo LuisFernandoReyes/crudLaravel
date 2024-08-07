0b48698b66d2fcc83e6e71a306e5eae239810cdd
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-08-07 17:54:39 -0600

    Typing methods

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -5,21 +5,23 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Note;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class NoteController extends Controller
 {
-    public function index(){
+    public function index():view {
         $notes= Note::all();
 
         return view('note.index', compact('notes'));
 
     }
 
-    public function create(){
+    public function create():view{
         return view('note.create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request):RedirectResponse{
         //better method because I used the rigth names in the form:
             Note::create($request->all());
             return redirect()->route('note.index');
@@ -40,19 +42,19 @@ class NoteController extends Controller
                 */
     }
 
-    public function edit(Note $note){
+    public function edit(Note $note):view{
         return view('note.edit', compact('note'));
     }
 
-    public function update(Request $request, Note $note){
+    public function update(Request $request, Note $note):RedirectResponse{
         $note->update($request->all());
         return redirect()->route('note.index');
     }
-    public function show(Note $note){
+    public function show(Note $note):view{
         return view('note.showNote', compact('note'));
 
     }
-    public function destroy(Note $note){
+    public function destroy(Note $note):RedirectResponse{
         $note->delete();
         return redirect()->route('note.index');
     }
